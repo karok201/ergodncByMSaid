ed4de42fe2d4b5d3ba0381d45cee133fb44f2e0d
Nikita Babichenko <babichenkona@gmail.com>
2022-05-24 08:12:15 +0000

    wip

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -14,9 +14,13 @@ class Office extends Model
     use HasFactory;
     use SoftDeletes;
 
+    const APPROVAL_PENDING = 1;
+    const APPROVAL_APPROVED = 2;
+    const APPROVAL_REJECTED = 3;
+
     protected $casts = [
-        'lat' => 'decimal',
-        'lng' => 'decimal',
+        'lat' => 'decimal:8',
+        'lng' => 'decimal:8',
         'approval_status' => 'integer',
         'hidden' => 'bool',
         'price_per_day' => 'integer',
