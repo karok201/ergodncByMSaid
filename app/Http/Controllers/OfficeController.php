f7f83922f000d1b29fd90c1d03e18ad75406e63f
Nikita Babichenko <babichenkona@gmail.com>
2022-05-24 13:05:25 +0000

    wip

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Resources\OfficeResource;
+use App\Models\Office;
+use App\Models\Reservation;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class OfficeController extends Controller
 {
-    //
+    public function index(): AnonymousResourceCollection
+    {
+        $offices = Office::query()
+            ->where('approval_status', Office::APPROVAL_APPROVED)
+            ->where('hidden', false )
+            ->when(request('host_id'), fn($q, $hostId) => $q->whereUserId($hostId))
+            ->when(request('user_id'),
+                fn($q, $userId)
+                    => $q->whereRelation('reservations', 'user_id', '=', $userId))
+            ->latest('id')
+            ->with(['images', 'tags', 'user'])
+            ->withCount(['reservations' => fn ($builder) => $builder->where('status', Reservation::STATUS_ACTIVE)])
+            ->paginate(20);
+
+        return OfficeResource::collection(
+            $offices
+        );
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\OfficeController;
 use App\Http\Controllers\TagController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Tags...
 Route::get('/tags', TagController::class);
+
+// Offices...
+Route::get('/offices', [OfficeController::class, 'index']);
