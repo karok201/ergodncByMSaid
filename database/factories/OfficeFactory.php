bd61bc285455a51c5a2a0dfc149be40f403fa857
Nikita Babichenko <babichenkona@gmail.com>
2022-05-24 07:57:41 +0000

    wip

---
+++ database/factories/OfficeFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OfficeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'lat' => $this->faker->latitude,
+            'lng' => $this->faker->longitude,
+            'address_line1' => $this->faker->address,
+            'approval_status' => 2,
+            'hidden' => false,
+            'price_per_day' => $this->faker->numberBetween(1_000,2_000)
+        ];
+    }
+}
